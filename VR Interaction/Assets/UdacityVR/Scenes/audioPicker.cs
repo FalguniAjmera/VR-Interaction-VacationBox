355da1f032f31a8099665d4fc028f1c9a1481050
FalguniAjmera <43901389+FalguniAjmera@users.noreply.github.com>
2020-09-08 00:14:00 +0530

    First commit
    added main scene

---
+++ VR Interaction/Assets/UdacityVR/Scenes/audioPicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioPicker : MonoBehaviour
+{
+	public AudioClip[] soundFiles;
+
+	public AudioSource soundSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int index = Random.Range(0, soundFiles.Length);
+        soundSource.clip = soundFiles[index];
+        soundSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR Interaction/Assets/fallUsingCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallUsingCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(transform.position.y > 0.6f){
+    		transform.Translate(0,-3.0f*Time.deltaTime,0,Space.World);
+
+    	}
+        
+    }
+}
